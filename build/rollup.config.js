29f319404cfe2d81986553fab616e2bee0a65618
zhongxingdou <zhongxingdou@gmail.com>
2016-06-28 14:47:33 +0800

    prepare test

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,9 +1,10 @@
 import babel from 'rollup-plugin-babel'
+import babelrc from 'babelrc-rollup'
 
 export default {
   entry: 'src/index.js',
   dest: 'dist/vueModel.js',
   format: 'umd',
   moduleName: 'VueModel',
-  plugins: [ babel() ]
+  plugins: [ babel(babelrc()) ]
 }
---
+++ test/modello.spec.js
@@ -0,0 +1,10 @@
+import should from 'should'
+import modello from '../src/index'
+
+describe('Modello', function () {
+  describe('Property Binding', function () {
+    it('test', function () {
+      modello.should.not.null
+    })
+  })
+})
